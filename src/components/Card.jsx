1f039f844fbcd4e61e62840ba34ab5272aa5f934
Emil Nilsson <emil@sharpfin.com>
2023-09-18 08:39:44 +0200

    white card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@
 
 function Card({ title, children, className, headerComponent, textClassName }) {
   return (
-    <div className={` border border-slate-300 rounded p-5 ${className}`}>
+    <div className={` bg-white border border-slate-300 rounded p-5 ${className}`}>
       {title &&
         <div className="flex justify-between ">
           <p className={`text-secondary text-sm ${textClassName}`}>{title}</p>
